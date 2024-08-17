3f6ee4df4eb3aa37c0c6f71ba55d1c4c6ee3629b
Jonathan Zhao <132334671+Saloushe12@users.noreply.github.com>
2024-08-17 15:52:56 -0400

    fixed date.js to accept UNIX dates

--- pages/api/date.js
+++ pages/api/date.js
@@ -1,29 +1,32 @@
-//Date String API endpoint
 module.exports = function(app) {
-    app.get("/api/:date?", function (req, res) {
-        let dateString = req.params.date;
-        let date;
-      
-        // If no date parameter is provided, return the current date
-        if (!dateString) {
+  app.get("/api/:date?", function (req, res) {
+      let dateString = req.params.date;
+      let date;
+
+      // If no date parameter is provided, return the current date
+      if (!dateString) {
           date = new Date();
-        }
-        else {
-          // Try to parse the date string
-          date = new Date(dateString);
-        }
-      
-        // If the date is invalid, return an error message
-        if (isNaN(date.getTime())) {
-          return res.json({Error: "Invalid date"});
-        }
-      
-        const unixTimestamp = date.getTime();
-        const utcDate = date.toUTCString();
-      
-        res.json({
+      } else {
+          // Check if the dateString is a valid Unix timestamp (i.e., a number)
+          if (/^\d+$/.test(dateString)) {
+              date = new Date(parseInt(dateString));
+          } else {
+              // Otherwise, treat it as a UTC date string
+              date = new Date(dateString);
+          }
+      }
+
+      // If the date is invalid, return an error message
+      if (isNaN(date.getTime())) {
+          return res.json({ error: "Invalid Date" });
+      }
+
+      const unixTimestamp = date.getTime();
+      const utcDate = date.toUTCString();
+
+      res.json({
           "unix": unixTimestamp,
           "utc": utcDate
-        });
       });
+  });
 };
